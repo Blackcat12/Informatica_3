b68a53df91c2ed168f46f94dc9495618aa7cae7d
Blackcat12 <diegogattari@Hotmail.it>
2016-12-31 01:12:17 +0100

    Update E_5_media.cpp

--- E_5_media.cpp
+++ E_5_media.cpp
@@ -10,9 +10,9 @@ ver: 1.0
 #include<stdlib.h>
 #include<math.h>
 int main(){
-	int n;
-	int n1;
-	int i;
+	int n;//variabile che contiene il numero dei numeri da inserire
+	int n1;//variabile che contiene il numero
+	int i;//contatore
 	int somma;
 	int media;
 	printf("inserisci il numero dei numeri\n");
@@ -20,9 +20,9 @@ int main(){
 	i=0;
 	somma=0;
 	while(i<n){
-		printf("inserisci il numero\n");
-		scanf("%d",n1);
-		somma=somma+n1;
+		printf("inserisci il numero\n");/*questo ciclo si ripete fin quando il contatore è minore di i e somma n alla somma*/
+		scanf("%d",n1);                    
+		somma=somma+n1;                   
 		i++;
 	}
 	media=somma/n;
