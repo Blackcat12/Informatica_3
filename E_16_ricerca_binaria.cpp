3ac33113f673175cd845c5dea353d38ded8a5dff
Blackcat12 <diegogattari@Hotmail.it>
2017-03-03 16:14:57 +0100

    Add files via upload

--- E_16_ricerca_binaria.cpp
+++ E_16_ricerca_binaria.cpp
@@ -1,34 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-	int n;
-	int min;
-	int max;
-	int trovato;
-	int cent;
+	int n;//numero da trovare
+	int min;//variabile che indica l'indice minore
+	int cont;//variabile che conta i cicli svolti
+	int max;//variabile che indica l'indice maggiore
+	int trovato;// indica se il numero è stato trovato
+	int cent;// centro
 	int num[10]={8,10,14,16,20,25,31,37,40,50};
-	printf("inserisci il numero da trovare:\n\n");
+	printf("inserisci il numero da trovare:\n\n");  // input numero da trovare
 	scanf("%d", & n);
 	min=0;
 	max=9;
 	trovato=0;
-	while((trovato==0)&&(min<=max)){
+	while((trovato==0)&&(min<=max)){  //ciclo che permette di controllare se il numero è presente nell'array
 		cent=(min+max)/2;
 		if(n==num[cent]){
-			trovato=1;	
+			trovato=1;
+			cont++;	
 		}else{
 			if(n>num[cent]){
 				min=cent+1;
+				cont++;
 			}else{
 				max=cent-1;
+				cont++;
 			}
 		}
 		printf("\nil centro e' %d\n", cent);
 	}
 	if(trovato==1){
-		printf("il numero e' presente nell'array nella seguente posizione %d", cent);
+		printf("\nil numero e' presente nell'array nella seguente posizione %d\n", cent);  //output se il numero è nell'array
+		printf("\nil numero è stato trovato in %d passaggi\n",cont);
 	}else{
-		printf("il numero non e' stato trovato nell'array");
+		printf("il numero non e' stato trovato nell'array");//output se il numero non è nell'array
 	}
 	
 
